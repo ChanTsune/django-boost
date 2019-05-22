b7e6ad25c3dff8c3ab7a219d3f342608799b7e10
taiki tsunekawa <s1f101702431@iniad.org>
2019-05-23 00:46:22 +0900

    [add] example

--- example/views.py
+++ example/views.py
@@ -1,10 +1,14 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, UpdateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy as _
 from django_boost.views.generic import JsonView
+from django.utils.timezone import now
 from django_boost.views.mixins import ReAuthenticationRequiredMixin
 from django_boost.views.mixins import RedirectToDetailMixin
+from django_boost.views.mixins import LimitedTermMixin
 from django_boost.http.response import Http409
 from .models import Customer
 from .forms import CustomerForm
@@ -35,4 +39,28 @@ class ReloginView(ReAuthenticationRequiredMixin, TemplateView):
     template_name = "example/index.html"
     extra_context = {"number": 1000}
     auth_unnecessary = 30
+
+
+class StartLimitView(LimitedTermMixin, TemplateView):
+    template_name = "example/index.html"
+
+    def get_start_datetime(self):
+        return now() + timedelta(hours=1)
+
+
+class EndLimitView(LimitedTermMixin, TemplateView):
+    template_name = "example/index.html"
+
+    def get_end_datetime(self):
+        return now() - timedelta(hours=1)
+
+
+class SELimitView(LimitedTermMixin, TemplateView):
+    template_name = "example/index.html"
+
+    def get_start_datetime(self):
+        return now() - timedelta(hours=1)
+
+    def get_end_datetime(self):
+        return now() + timedelta(hours=1)
 
