c71d4a0ba4e536d0ec725080df9bb83be3bc1012
taiki tsunekawa <s1f101702431@iniad.org>
2019-06-12 22:18:43 +0900

    [add] urlconf

--- example/urls.py
+++ example/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('customer/<int:pk>/update/', views.CustomerUpdateView.as_view(), name='customer_update'),
     path('json/', views.JsonView.as_view(extra_response_data={"json": True})),
     path('json/2/', views.JsonView.as_view(extra_response_data={"json": True}, strictly=True)),
+    path('start/',views.StartLimitView.as_view()),
+    path('end/',views.EndLimitView.as_view()),
+    path('se/',views.SELimitView.as_view()),
 ]
