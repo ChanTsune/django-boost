4b1962f7c5826af615f17383d2944b7e5d3bf90e
taiki tsunekawa <s1f101702431@iniad.org>
2019-07-01 10:54:42 +0900

    [white space]

--- example/admin.py
+++ example/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Customer
 # Register your models here.
 
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('id', 'color', 'registered_at')
--- example/models.py
+++ example/models.py
@@ -3,7 +3,8 @@ from django_boost.models.mixins import JsonMixin
 from django_boost.models.fields import ColorCodeFiled
 # Create your models here.
 
-class Customer(JsonMixin,models.Model):
+
+class Customer(JsonMixin, models.Model):
     name = models.CharField(max_length=64)
     registered_at = models.DateField(auto_now_add=True)
     color = ColorCodeFiled(upper=True)
