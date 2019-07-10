01f356e7ab9c81711518417717d245d19bd3d301
taiki tsunekawa <s1f101702431@iniad.org>
2019-07-11 01:45:18 +0900

    [fix] migration file

--- django_boost/migrations/0001_initial.py
+++ django_boost/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0009_alter_user_last_name_max_length'),
+        ('auth', '__first__'),
     ]
 
     operations = [
