2ebe9786b058e54ec1803a8f63a505f7950f6a24
taiki tsunekawa <s1f101702431@iniad.org>
2019-08-18 19:12:22 +0900

    [feat] logical deletion

---
+++ django_boost/models/manager.py
@@ -0,0 +1,16 @@
+from django.db.models.manager import Manager
+
+from django_boost.models.query import LogicalDeletionQuerySet
+
+
+class LogicalDeletionManager(Manager):
+    delete_flag_field = "deleted_at"
+
+    def get_deleted_flag_field_name(self):
+        return self.delete_flag_field
+
+    def get_queryset(self):
+        return LogicalDeletionQuerySet(self.model)
+
+    def delete(self, hard=False):
+        return self.get_queryset.delete(hard=hard)
--- django_boost/models/mixins.py
+++ django_boost/models/mixins.py
@@ -1,7 +1,9 @@
 import uuid
 
 from django.db import models
+from django.utils.timezone import now
 
+from django_boost.models.manager import LogicalDeletionManager
 from django_boost.utils.functions import json_to_model, model_to_json
 
 
@@ -32,3 +34,24 @@ class TimeStampModelMixin(models.Model):
         abstract = True
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class LogicalDeletionMixin(models.Model):
+    """Provide logical delete."""
+
+    deleted_at = models.DateTimeField(
+        verbose_name="deleted_at", blank=True, null=True, default=None)
+
+    class Meta:
+        abstract = True
+
+    objects = LogicalDeletionManager()
+
+    def get_deleted_value(self):
+        return now()
+
+    def delete(self, using=None, keep_parents=False, hard=False):
+        if hard:
+            return super().delete(using=using, keep_parents=keep_parents)
+        self.deleted_at = self.get_deleted_value()
+        return self.save()
