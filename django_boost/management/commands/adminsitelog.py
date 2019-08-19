3572fc5189075980475920f3f344ae821ceda401
tsunekwataiki <tsunekwataiki@LB-MBP.local>
2019-08-19 18:01:16 +0900

    [change] print format

--- django_boost/management/commands/adminsitelog.py
+++ django_boost/management/commands/adminsitelog.py
@@ -7,19 +7,34 @@ from django_boost.core import get_version
 
 class Command(BaseCommand):
 
-    def print_log(self,log):
-        self.stdout.write(str(log))
+    def print_log(self, log):
+        fmt = "{action} | {object} by {user} | {time}"
+        fmap = {}
+        if log.is_addition():
+            fmap["action"] = self.style.SUCCESS("Added")
+            fmap["object"] = log.object_repr
+        elif log.is_change():
+            fmap["action"] = self.style.WARNING("Changed")
+            fmap["object"] = "%s - %s" % (log.object_repr,
+                                          log.get_change_message())
+        else:  # log.is_deletion
+            fmap["action"] = self.style.ERROR("Deleted")
+            fmap["object"] = log.object_repr
+        fmap["user"] = log.user.username
+        fmap["time"] = log.action_time
+        self.stdout.write(fmt.format_map(fmap))
 
     def add_arguments(self, parser):
         parser.add_argument('-d', '--delete', action='store_true')
-        parser.add_argument('--username', nargs="+", type=str, default=None)
+        parser.add_argument('--filter', nargs="+", type=str, default=[])
+        parser.add_argument('--exclude', nargs="+", type=str, default=[])
+        parser.add_argument('--order_by', nargs="+",
+                            type=str, default=["action_time"])
 
     def handle(self, *args, **options):
         self.stdout.write(str(args))
         self.stdout.write(str(options))
         queryset = LogEntry.objects.all()
-        if options['username']:
-            queryset = queryset.filter(user__username__in=options['username'])
         if queryset.count() == 0:
             self.stderr.write('No logs')
             return
@@ -27,15 +42,12 @@ class Command(BaseCommand):
             self.print_log(log)
         if options['delete']:
             answer = input(_("Do you want to delete these logs [y/n]?"))
-            if answer.lower() in ["y","yes"]:
+            if answer.lower() in ["y", "yes"]:
                 queryset.delete()
                 self.stdout.write('delete complete')
             else:
                 self.stderr.write('operation canceled')
             return
-        from pprint import pprint
-        pprint(dir(LogEntry))
-
 
     def get_version(self):
         return get_version()
