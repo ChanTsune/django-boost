329aa6ab0da56c30a89bf78b0c780a2474da0fd3
tsunekwataiki <tsunekwataiki@LB-MBP.local>
2019-08-20 16:26:09 +0900

    [feat] simple filter

--- django_boost/management/commands/adminsitelog.py
+++ django_boost/management/commands/adminsitelog.py
@@ -7,6 +7,29 @@ from django_boost.core import get_version
 
 class Command(BaseCommand):
 
+    COMPARISON_OPERATION = {"<=": "lte",
+                            ">=": "gte",
+                            "=": "exact",
+                            "<": "lt",
+                            ">": "gt", }
+
+    def _parse_filter(self, condition):
+        for op in self.COMPARISON_OPERATION.keys():
+            field, op, value = condition.partition(op)
+            op = self.COMPARISON_OPERATION.get(op, None)
+            if op is not None:
+                return {"%s__%s" % (field, op): value}
+        raise Exception("""
+        Unsupported operation '%s'
+        --filter and --exclude supported 
+        """ % (field, ",".join(self.COMPARISON_OPERATION.keys())))
+
+    def parse_filter(self, conditions):
+        parsed = {}
+        for condition in conditions:
+            parsed.update(self._parse_filter(condition))
+        return parsed
+
     def print_log(self, log):
         fmt = "{action} | {object} by {user} | {time}"
         fmap = {}
@@ -35,6 +58,8 @@ class Command(BaseCommand):
         self.stdout.write(str(args))
         self.stdout.write(str(options))
         queryset = LogEntry.objects.all()
+        queryset = queryset.filter(**self.parse_filter(options['filter']))
+        queryset = queryset.exclude(**self.parse_filter(options['exclude']))
         if queryset.count() == 0:
             self.stderr.write('No logs')
             return
