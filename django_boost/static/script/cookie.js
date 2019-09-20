69dacc6a668a51710bb352309761e0d6e5866185
taiki tsunekawa <s1f101702431@iniad.org>
2019-09-20 13:50:30 +0900

    Update cookie.js

--- django_boost/static/script/cookie.js
+++ django_boost/static/script/cookie.js
@@ -1,4 +1,4 @@
-var Cookies = function(){}
+var Cookies = function () { }
 Cookies.get = function (key) {
     var cookies = document.cookie;
     var cookiesArray = cookies.split('; ');
@@ -35,6 +35,85 @@ Cookies.delete = function (key) {
     }
     return undefined;
 }
-Cookies.has = function(key){
+Cookies.reset = function (path) {
+    Cookies.keys().forEach(function (k) {
+        Cookies.delete(k);
+    })
+}
+Cookies.has = function (key) {
     return (Cookies.get(key) != undefined);
+}
+Cookies.append = function (key, value, attributes) {
+    if (Cookies.has(key)) {
+        return Cookies.set(key, Cookies.get(key) + "&|&" + value, attributes);
+    } else {
+        return Cookies.set(key, value, attributes);
+    }
+}
+Cookies.remove = function (key, value, attributes) {
+    if (Cookies.has(key)) {
+        var removed = new Array;
+        Cookies.getList(key).forEach(function (v) {
+            if (v != value) {
+                removed.push(v);
+            }
+        })
+        Cookies.set(key, removed.join("&|&"), attributes);
+    }
+}
+Cookies.contain = function (key, value) {
+    if (Cookies.has(key)) {
+        var contained = false;
+        Cookies.getList(key).forEach(function (v) {
+            if (v == value) {
+                contained = true;
+            }
+        })
+        return contained;
+    }
+    return false;
+}
+Cookies.getList = function (key) {
+    if (Cookies.has(key)) {
+        return Cookies.get(key).split("&|&");
+    }
+    return [];
+}
+Cookies.fromJson = function (object, attributes) {
+    Object.keys(object).forEach(function (k) {
+        Cookies.set(k, object[k], attributes);
+    })
+}
+Cookies.fromJsonString = function (string, attributes) {
+    Cookies.fromJson(JSON.parse(string), attributes);
+}
+Cookies.asJsonString = function () {
+    return JSON.stringify(Cookies.asJson());
+}
+Cookies.asJson = function () {
+    var obj = {};
+    Cookies.keys().forEach(function (k) {
+        obj[k] = Cookies.get(k);
+    })
+    return obj;
+}
+Cookies.keys = function () {
+    var keys = new Array;
+    var cookies = document.cookie;
+    var cookiesArray = cookies.split('; ');
+    for (var c of cookiesArray) {
+        var cArray = c.split('=');
+        keys.push(cArray[0]);
+    }
+    return keys;
+}
+Cookies.values = function () {
+    var values = new Array;
+    var cookies = document.cookie;
+    var cookiesArray = cookies.split('; ');
+    for (var c of cookiesArray) {
+        var cArray = c.split('=');
+        values.push(cArray[1]);
+    }
+    return values;
 }
