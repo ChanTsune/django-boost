b8b2d78067213a44a06fec0a279b947b1a46b01c
taiki tsunekawa <s1f101702431@iniad.org>
2019-09-28 03:23:16 +0900

    [refactor] rename

--- example/views/__init__.py
+++ example/views/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView, UpdateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy as _
+from django.urls import reverse_lazy
 from django.utils.timezone import now
 from django_boost.views.generic import JsonView, ModelCRUDViews
 from django_boost.views.mixins import ReAuthenticationRequiredMixin
@@ -22,7 +22,7 @@ class IndexView(CreateView):
     template_name = "example/index.html"
     extra_context = {"number": -10}
     form_class = CustomerForm
-    success_url = _('index')
+    success_url = reverse_lazy('index')
 
 
 class CustomerUpdateView(RedirectToDetailMixin, UpdateView):
