b2d0fb5462b00b392ad495d697febd453138d51a
taiki tsunekawa <s1f101702431@iniad.org>
2019-09-28 17:38:14 +0900

    [update] test validatiors

--- django_boost/tests/test_validatiors.py
+++ django_boost/tests/test_validatiors.py
@@ -5,16 +5,25 @@ from django_boost.validators import (
     validate_color_code, validate_json, validate_uuid4)
 
 
-class ValidatorTest(TestCase):
+class TestValidator(TestCase):
 
     def test_validate_color_code(self):
         with self.assertRaises(ValidationError):
             validate_color_code("00FF11")
+        
+        with self.assertRaises(ValidationError):
+            validate_color_code("#R00000")
 
     def test_validate_json(self):
         with self.assertRaises(ValidationError):
             validate_json('{"a":"apple",}')
+        
+        with self.assertRaises(ValidationError):
+            validate_json("{'a':'apple'}")
 
     def test_validate_uuid4(self):
         with self.assertRaises(ValidationError):
             validate_uuid4("59cF05e3-fb29-4be8-af18-da9c94b1964d")
+        
+        with self.assertRaises(ValidationError):
+            validate_uuid4("5de21727-8c45-4380-a44d-adc370-e6288")
