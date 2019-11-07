ed5f3c8345886f1e1c7aa12a70f41898eef4516c
taiki tsunekawa <s1f101702431@iniad.org>
2019-11-07 14:15:48 +0900

    [update] docs

--- tests/tests/forms_fields/tests.py
+++ tests/tests/forms_fields/tests.py
@@ -2,7 +2,7 @@ from django_boost.test import TestCase
 
 
 class TestField(TestCase):
-    
+
     def test_invert_boolean_field(self):
         from django.forms import Form
         from django_boost.forms.fields import InvertBooleanField
@@ -10,11 +10,8 @@ class TestField(TestCase):
         class SampleForm(Form):
             sample = InvertBooleanField(required=False)
 
-        form = SampleForm({'sample': False})
-        self.assertTrue(form.is_valid())
-        self.assertTrue(form.cleaned_data['sample'])
-
-        form = SampleForm({'sample': True})
-
-        self.assertTrue(form.is_valid())
-        self.assertFalse(form.cleaned_data['sample'])
+        case = [False, True]
+        for i in case:
+            form = SampleForm({'sample': i})
+            self.assertTrue(form.is_valid())
+            self.assertEqual(form.cleaned_data['sample'], not i)
