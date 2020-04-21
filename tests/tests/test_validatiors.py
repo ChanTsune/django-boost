cb856d377c46f2a00bd097a42bae637feb9f7407
ChanTsune <s1f101702431@iniad.org>
2020-04-21 15:02:56 +0900

    Add Test

--- tests/tests/test_validatiors.py
+++ tests/tests/test_validatiors.py
@@ -5,7 +5,9 @@ from django.core.exceptions import ValidationError
 
 from django_boost.test import TestCase
 from django_boost.validators import (
-    validate_color_code, validate_json, validate_uuid4)
+    validate_color_code, validate_json, validate_uuid4,
+    ContainAnyValidator
+)
 
 TEST_DATA = [
     (validate_color_code, "#001122", None),
@@ -20,6 +22,10 @@ TEST_DATA = [
     (validate_uuid4, str(uuid4()), None),
     (validate_uuid4, "59cF05e3-fb29-4be8-af18-da9c94b1964d", ValidationError),
     (validate_uuid4, "5de21727-8c45-4380-a44d-adc370-e6288", ValidationError),
+
+    (ContainAnyValidator("1"), "123", None),
+    (ContainAnyValidator("02"), "123", None),
+    (ContainAnyValidator("4"), "123", ValidationError),
 ]
 
 
