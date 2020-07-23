6cc0e668c4abc04f9976789d9b4fa3b27b0b5263
taiki tsunekawa <s1f10170243@iniad.org>
2020-07-23 16:10:45 +0900

    Add TestLogicalDeletionManager

--- tests/tests/logical_deletion/tests.py
+++ tests/tests/logical_deletion/tests.py
@@ -46,3 +46,30 @@ class TestLogicalDeletionMixin(TestCase):
         item = self.model.objects.get(name="0")
         item.delete()
         self.assertEqual(len(self.model.objects.dead()), 1)
+
+class TestLogicalDeletionManager(TestCase):
+    from .models import LogicalDeletionModel
+
+    model = LogicalDeletionModel
+
+    def _register_items(self, *args):
+        for item in args:
+            self.model.objects.create(name=item)
+
+    def _hard_delete(self):
+        for i in self.model.all():
+            i.delete(hard=True)
+
+    def test_delete(self):
+        self._register_items(*[str(i) for i in range(10)])
+        self.model.objects.delete()
+        self.assertEqual(len(self.model.objects.dead()), 10)
+        self._hard_delete()
+
+    def test_hard_delete(self):
+        self._register_items(*[str(i) for i in range(10)])
+        self.model.objects.delete(hard=True)
+        self.assertEqual(len(self.model.objects.dead()), 0)
+        self.assertEqual(len(self.model.objects.alive()), 0)
+        self.assertEqual(len(self.model.objects.all()), 0)
+        self._hard_delete()
