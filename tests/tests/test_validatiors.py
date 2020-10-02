deec83051c5d2a13fd43e0ad434381ea6a0cbcbe
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-10-03 01:39:28 +0900

    Update test_validatiors.py

--- tests/tests/test_validatiors.py
+++ tests/tests/test_validatiors.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 
 from django_boost.test import TestCase
 from django_boost.validators import (
-    ContainAnyValidator,
+    ContainAnyValidator, JsonValidator,
     validate_color_code, validate_json, validate_uuid4,
 )
 
@@ -18,6 +18,7 @@ TEST_DATA = [
     (validate_json, '{"a":"apple"}', None),
     (validate_json, '{"a":"apple",}', ValidationError),
     (validate_json, "{'a':'apple'}", ValidationError),
+    (validate_json, 1, TypeError),
 
     (validate_uuid4, str(uuid4()), None),
     (validate_uuid4, "59cF05e3-fb29-4be8-af18-da9c94b1964d", ValidationError),
@@ -43,3 +44,14 @@ class TestValidator(TestCase):
                         validator(value)
                 else:
                     self.assertEqual(expected, validator(value))
+
+    def test_validator_message(self):
+        cases = [
+            (ContainAnyValidator("1", "custom message with %s"), "0", "custom message with 1"),
+            (JsonValidator("custom message"), "", "custom message"),
+        ]
+        for validator, value, message in cases:
+            name = validator.__class__.__name__
+            with self.subTest(name, value=value):
+                with self.assertRaisesMessage(ValidationError, message):
+                    validator(value)
