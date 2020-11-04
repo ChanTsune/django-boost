ee6a75906644ced0d313b16f436d3b25d7cc2c8b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-11-04 22:12:33 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'License :: OSI Approved :: MIT License',
     ],
 )
