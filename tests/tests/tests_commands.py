156b0aa83cf8c89769270a6a9b329d39c61ce6d6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-11-04 21:56:25 +0900

    Update tests_commands.py

--- tests/tests/tests_commands.py
+++ tests/tests/tests_commands.py
@@ -19,8 +19,29 @@ class TestStartAppPlus(TestCase):
     def test_call_command(self):
         call_command('startapp_plus', 'app_for_test')
 
+    @classmethod
+    def tearDownClass(self):
+        import os
+        from shutil import rmtree
+        from django.conf import settings
+
+        for f in ['app_for_test']:
+            fp = os.path.join(settings.BASE_DIR, f)
+            if os.path.exists(fp):
+                rmtree(fp)
+
 
 class TestSupportHeroku(TestCase):
 
     def test_call_command(self):
         call_command('support_heroku')
+
+    @classmethod
+    def tearDownClass(self):
+        import os
+        from django.conf import settings
+
+        for f in ['Procfile', 'runtime.txt', 'requirements.txt']:
+            fp = os.path.join(settings.BASE_DIR, f)
+            if os.path.exists(fp):
+                os.remove(fp)
