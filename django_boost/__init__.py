4b4089624e47ef30e3b837bf4f3a91f3f1226e07
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-12-14 23:56:02 +0900

    default_app_config application variable is deprecated in Django 3.2

--- django_boost/__init__.py
+++ django_boost/__init__.py
@@ -1,3 +1,4 @@
+import django
 from django_boost.utils.version import get_version
 
 VERSION = (1, 7, 2, 'final', 0)
@@ -5,4 +6,6 @@ VERSION = (1, 7, 2, 'final', 0)
 
 __version__ = get_version()
 
-default_app_config = 'django_boost.apps.DjangoBoostConfig'
+
+if django.VERSION < (3, 2):
+    default_app_config = 'django_boost.apps.DjangoBoostConfig'
