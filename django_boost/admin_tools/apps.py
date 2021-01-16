332a17ba604be25d3f3bcbedf8f5a55c4dfa9052
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-01-17 01:26:58 +0900

    Revert "Startapp admin_tools"
    This reverts commit 8c44922ab77ebba81f4670f683d852d4415108cc.

--- django_boost/admin_tools/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminToolsConfig(AppConfig):
-    name = 'admin_tools'
-
-    def ready(self):
-        # Implement your startup processing
-        pass
