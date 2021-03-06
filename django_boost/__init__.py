ff4dd411dd198c95c4a70884a647383d97709ab5
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-03-07 00:28:31 +0900

    Fix setup error
    Fixed `python setup.py install` error

--- django_boost/__init__.py
+++ django_boost/__init__.py
@@ -1,4 +1,3 @@
-import django
 from django_boost.utils.version import get_version
 
 VERSION = (1, 8, 0, 'alpha', 0)
@@ -6,6 +5,9 @@ VERSION = (1, 8, 0, 'alpha', 0)
 
 __version__ = get_version()
 
-
-if django.VERSION < (3, 2):
-    default_app_config = 'django_boost.apps.DjangoBoostConfig'
+try:
+    import django
+    if django.VERSION < (3, 2):
+        default_app_config = 'django_boost.apps.DjangoBoostConfig'
+except ImportError:
+    pass
