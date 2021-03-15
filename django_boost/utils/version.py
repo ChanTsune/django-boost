ec705451d9e2ba003f7b21d89b540354ed542483
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-03-15 14:18:52 +0900

    Refactor get_docs_version

--- django_boost/utils/version.py
+++ django_boost/utils/version.py
@@ -44,5 +44,4 @@ def get_docs_version(version=None):
     version = get_complete_version(version)
     if version[3] != 'final':
         return 'dev'
-    else:
-        return '%d.%d' % version[:2]
+    return '%d.%d' % version[:2]
