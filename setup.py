778ff5d4e5ddb9dd891f552128c5689d97385841
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2022-01-23 21:48:28 +0900

    Drop python 3.6, 3.7 support

--- setup.py
+++ setup.py
@@ -74,10 +74,9 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Framework :: Django',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
