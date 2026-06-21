bf383501d50f7b74ba0e9559ee204e53bb66f3d9
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2026-06-21 16:50:38 +0900

    :warning: Deprecate the support_heroku management command

--- tests/tests/test_deprecations.py
+++ tests/tests/test_deprecations.py
@@ -1,3 +1,5 @@
+from django.core.management import call_command
+
 from django_boost.test import TestCase
 
 
@@ -14,3 +16,20 @@ class JsonFieldDeprecationTests(TestCase):
         from django_boost.models.fields import JsonField
         with self.assertWarnsRegex(DeprecationWarning, r"Django 3\.1"):
             JsonField()
+
+
+class SupportHerokuDeprecationTests(TestCase):
+    def test_support_heroku_warns(self):
+        with self.assertWarnsRegex(DeprecationWarning, r"support_heroku.*django-boost 3\.0"):
+            call_command("support_heroku")
+
+    @classmethod
+    def tearDownClass(cls):
+        import os
+        from django.conf import settings
+
+        for name in ["Procfile", "runtime.txt", "requirements.txt"]:
+            fp = os.path.join(settings.BASE_DIR, name)
+            if os.path.exists(fp):
+                os.remove(fp)
+        super().tearDownClass()
