374513de7929157779f8633e204771fa32adb04b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2026-06-27 13:15:03 +0900

    :recycle: Point example views at django.views.generic
    Prep for deprecating the django_boost generic-view aliases: the example
    blog and exception views used them only as plain generic views, so import
    them from django.views.generic instead.

--- example/views/exception.py
+++ example/views/exception.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import Any, NoReturn
 
-from django_boost.views.generic import TemplateView
+from django.views.generic import TemplateView
+
 from django_boost.http.response import Http415
 
 
