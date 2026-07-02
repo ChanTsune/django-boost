2f0d61b43118f7bcba3fa7e5ff512a19acafe6dc
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2026-07-03 08:36:43 +0900

    :bug: Let looplast/loopfirstlast accept any iterable
    Both computed their last index with len(), so a true iterator such as a
    generator raised TypeError, even though the docstrings advertise 'iterator
    object' and loopfirst already accepts one. Materialize the input to a list
    only when it is not already a sized collection (lists, QuerySets, etc. are
    iterated in place), and widen the type hint from Sequence to Iterable.
    
    Refs: https://github.com/ChanTsune/django-boost/issues/164

--- django_boost/utils/functions/loop.py
+++ django_boost/utils/functions/loop.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from collections.abc import Iterable, Iterator, Sequence
+from collections.abc import Collection, Iterable, Iterator
 from typing import TypeVar
 
 _T = TypeVar("_T")
@@ -18,27 +18,29 @@ def loopfirst(iterable: Iterable[_T]) -> Iterator[tuple[bool, _T]]:
         yield i == 0, val
 
 
-def looplast(iterable: Sequence[_T]) -> Iterator[tuple[bool, _T]]:
+def looplast(iterable: Iterable[_T]) -> Iterator[tuple[bool, _T]]:
     """
     Loop util.
 
     Yield True when the last element of the given iterator object,
     False otherwise.
     """
-    it = iterable
-    last_index = len(it) - 1
-    for i, val in enumerate(it):
+    items: Collection[_T] = (
+        iterable if isinstance(iterable, Collection) else list(iterable))
+    last_index = len(items) - 1
+    for i, val in enumerate(items):
         yield i == last_index, val
 
 
-def loopfirstlast(iterable: Sequence[_T]) -> Iterator[tuple[bool, _T]]:
+def loopfirstlast(iterable: Iterable[_T]) -> Iterator[tuple[bool, _T]]:
     """
     A function combining `firstloop` and` lastloop`.
 
     Yield True if the first and last element of the iterator object,
     False otherwise.
     """
-    it = iterable
-    last_index = len(it) - 1
-    for i, val in enumerate(it):
+    items: Collection[_T] = (
+        iterable if isinstance(iterable, Collection) else list(iterable))
+    last_index = len(items) - 1
+    for i, val in enumerate(items):
         yield i == 0 or i == last_index, val
