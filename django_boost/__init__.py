029636f1c94379c8367269b0c8a0d95eca1e9e96
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2026-07-16 11:22:00 +0900

    :pencil: Bump to 3.3.1a0

--- django_boost/__init__.py
+++ django_boost/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from django_boost.utils.version import get_version
 
-VERSION = (3, 3, 0, 'final', 0)
+VERSION = (3, 3, 1, 'alpha', 0)
 
 
 __version__ = get_version()
